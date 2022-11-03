048d5c83ca852bfa97b34707d33b5e8026f30b69
mlNothing <mlingSame@gmail.com>
2022-11-03 14:07:55 +0800

    main.js

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import uView from "uview-ui";
-import api from './utils/api.js';
+import request from './utils/api.js';
+Vue.prototype.$H = request;
 Vue.use(uView);
-Vue.prototype.api = api;
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
