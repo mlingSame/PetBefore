45e81d0d6bde7e9a146a8564858af0f78320e64a
mlNothing <mlingSame@gmail.com>
2022-11-03 13:50:23 +0800

    api

--- utils/api.js
+++ utils/api.js
@@ -1,29 +1,66 @@
 
-const baseUrl = 'http://172.20.10.2:8080' 
-function req(method,url, data) {
-	// const name = uni.getStorageSync('name');
-	// const toker = uni.getStorageSync('toker');
-	
-	return new Promise((resolve, reject) => {
-		uni.request({
-			method: method,
-			url: baseUrl+url,
-			// url: urls+url+"?name="+name+"&toker="+toker,
-			data: data,
-			sslVerify:false,
-			success: (res) => {
-				if(res){
-					resolve(res);
+const baseUrl = 'http://192.168.0.103:8080' 
+export default {
+	request(options = {}) {
+		return new Promise((resolve, reject) => {
+			let url = options.url;
+			if (url.indexOf("http://") == -1 && url.indexOf("https://") == -1) {
+				options.url = baseUrl+url;
+			}
+			options.header.token = uni.getStorageSync("token");
+			options.complete = (response) => {
+				if (response.statusCode == 200 || response.statusCode == 0) {
+					if (response.data.code == 401 || response.data.code == 420) {
+						// #ifdef 
+						uni.navigateTo({
+							url: "/pages/user/login"
+						})
+						// #endif
+					}
+
+					if (response.data.code == 500) {
+						uni.showToast({
+							title: response.data.msg,
+							icon: "none",
+							duration: 2000
+						});
+					}
+					resolve(response.data)
+				} else {
+					uni.showToast({
+						title: '请求异常！',
+						icon: "none"
+					});
 				}
-			},
-			fail: (err) => {
-				console.log(err);
-				reject(err);
-			}	
+			}
+
+			uni.request(options)
 		})
-	})
-}
-module.exports = {
-	req
-}
+	},
+
+	post(url, data = {}, header = {}) {
+
+		let options = {
+			url: url,
+			data: data,
+			header: header,
+			method: "POST",
+			sslVerify:false
+		}
+
+		return this.request(options);
+	},
+
+	get(url, data = {}, header = {}) {
+		let options = {
+			url: url,
+			data: data,
+			header: header,
+			sslVerify:false,
+		}
+
+		return this.request(options);
+	}
+};
+
 
