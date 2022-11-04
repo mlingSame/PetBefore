6e4c609fe773d640b2b021f67296460454f2db1b
mlNothing <mlingSame@gmail.com>
2022-11-04 20:14:30 +0800

    基础配置

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ import App from './App'
 import Vue from 'vue'
 import uView from "uview-ui";
 import request from './utils/api.js';
+import store from "./store"
+Vue.prototype.$store = store;
 Vue.prototype.$H = request;
 Vue.use(uView);
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+    ...App,
+	store
 })
 app.$mount()
 // #endif
