b8ea30bd34a13b1c09be425316953b4b573fa8a0
mlNothing <mlingSame@gmail.com>
2022-11-05 00:10:21 +0800

    基础配置

---
+++ pages/user/updateFace.vue
@@ -0,0 +1,18 @@
+<template>
+		<view>
+			<u-button @click="hello()">测试</u-button>
+		</view>
+</template>
+<script>
+	export default {
+		methods: {
+			hello(){
+				
+				console.log(uni.getStorageSync("userInfo"));
+			}
+
+		}
+	}
+</script>
+
+
