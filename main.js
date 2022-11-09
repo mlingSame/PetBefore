f25d49022f85b3df298bfff90176fe35bf2a4be3
mlNothing <mlingSame@gmail.com>
2022-11-09 23:30:04 +0800

    主页面

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ import App from './App'
 // #ifndef VUE3
 import Vue from 'vue'
 import uView from "uview-ui";
+Vue.use(uView);
 import api from './utils/api.js';
 import store from "./store"
+
 Vue.prototype.$store = store;
 Vue.prototype.$H = api;
-Vue.use(uView);
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,23 +1,78 @@
 <template>
-	<view class="wrap">
-		<u-search class="search":clearabled="true"></u-search>	
-		<swiper class="swiper" 
-				indicator-dots="true" 
-				autoplay="true" 
-				interval="5000" 
-				duration="1500"	>
-				<swiper-item v-for="(item , index) in list" :key="index">
+	<view>
+		<view class="header">
+			<u-search class="search" :clearabled="true"></u-search>
+			<swiper class="swiper" indicator-dots="true" autoplay="true" interval="5000" duration="1500">
+				<swiper-item v-for="(item , index) in listSwiper" :key="index">
 					<image :src="item.image" mode="aspectFill"></image>
 				</swiper-item>
 			</swiper>
+		</view>
+		<view class="content-class">
+			<view class="card">
+				<u-line color="#e4e7ed" />
+				<view>宠物商店</view>
+				<u-row gutter="16">
+					<u-col span="4">
+						<view class="demo-layout bg-purple">
+							<view class="titleCard">猫猫</view>
+							<image class="imgcard" src="../../static/img/card/cat.jpg"></image>
+						</view>
+					</u-col>
+					<u-col span="4">
+						<view class="demo-layout bg-purple-light">
+							<view class="titleCard">狗狗</view>
+							<image class="imgcard" src="../../static/img/card/dog.jpg"></image>
+						</view>
+					</u-col>
+					<u-col span="4">
+						<view class="demo-layout bg-purple">
+							<view class="titleCard">兔兔</view>
+							<image class="imgcard" src="../../static/img/card/tu.jpg"></image>
+						</view>
+					</u-col>
+				</u-row>
+				<u-row gutter="16">
+					<u-col span="4">
+						<view class="demo-layout bg-purple">
+							<view class="titleCard">鼠鼠</view>
+							<image class="imgcard" src="../../static/img/card/rat.jpg"></image>
+						</view>
+					</u-col>
+					<u-col span="4">
+						<view class="demo-layout bg-purple-light">
+							<view class="titleCard">鸡鸡</view>
+							<image class="imgcard" src="../../static/img/card/ji.jpg"></image>
+						</view>
+					</u-col>
+					<u-col span="4">
+						<view class="demo-layout bg-purple">
+							<view class="titleCard">
+								<u-button @click="petInfoAll" >其他</u-button>
+							</view>
+							<image class="imgcard" src="../../static/img/card/ji.jpg"></image>
+						</view>
+					</u-col>
+				</u-row>
+			</view>
+			<view  class="card food">
+				<u-line color="#e4e7ed" />
+				<u-text>宠物食品</u-text>
+			</view>
+			<view 	class="card hospital">
+				<u-line color="#e4e7ed" />
+				<u-text>宠物医院</u-text>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
 	export default {
+		name: "petStore",
 		data() {
 			return {
-				list: [{
+				listSwiper: [{
 						image: '../../static/img/1.png',
 						title: '蒹葭苍苍，白露为霜。所谓伊人，在水一方'
 					},
@@ -30,27 +85,78 @@
 						title: '蒹葭萋萋，白露未晞。所谓伊人，在水之湄'
 					}
 				]
-			}
+			};
 		},
-		
-		methods: {
-			
+		methods:{
+			petInfoAll(){
+				console.log(this.$u.config.v);
+
+				uni.redirectTo({
+					url:'/pages/index/petInfoAll'
+				})
+			}
 		}
 	}
 </script>
 
-<style lang="scss" scoped>
-	.swiper{
+<style>
+	.swiper {
 		height: 400upx;
 	}
-	
-	// swiper-item 里面的图片高度
-	swiper-item image{
+
+	swiper-item image {
 		width: 100%;
 		height: 400rpx;
 	}
-	.search{
+
+	.search {
 		margin-bottom: 50rpx;
 	}
 
+	.content-class {
+		margin-top: 20upx;
+	}
+
+	.card {
+		height: 550upx;
+		background-color: '#e6bab2';
+	}
+
+	.wrap {
+		padding: 24rpx;
+	}
+
+	.u-row {
+		margin: 40rpx 0;
+	}
+
+	.demo-layout {
+		margin-left: 10upx;
+		width: 200rpx;
+		height: 200rpx;
+		border-radius: 5%;
+	}
+
+	.bg-purple {
+		background: #d3dce6;
+	}
+
+	.bg-purple-light {
+		background: #e5e9f2;
+	}
+
+	.bg-purple-dark {
+		background: #99a9bf;
+	}
+
+	.imgcard {
+		width: 120upx;
+		height: 120upx;
+		margin-left: 30upx;
+		border-radius: 50%;
+	}
+	.titleCard{
+		font-size: 40upx;
+		color: red;
+	}
 </style>
